f34b337ef30d47abb511b52f68a19e7938d13374
kambe <50097614+nobuaki0331@users.noreply.github.com>
2020-12-27 20:08:49 +0900

    [fix] リファクタ

--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,11 @@ use App\Http\Controllers\HogeController;
 use App\Http\Controllers\LogController;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-// */
-// Route::group(['middleware' => ['auth:api']], function(){
-//     Route::get('hoge', [HogeController::class, 'index']);
-//     Route::get('log/playlog', [LogController::class, 'play']);
-//     Route::get('account', [UserController::class, 'index']);
-// });
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['middleware' => ['auth:api']], function(){
+    Route::get('log/playlog', [LogController::class, 'play']);
+    Route::get('account', [UserController::class, 'index']);
 });
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
