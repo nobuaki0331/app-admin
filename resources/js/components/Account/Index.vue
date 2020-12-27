c6ac2c89ce8601f86bf3218d2b4df7e7c1d617be
kambe <50097614+nobuaki0331@users.noreply.github.com>
2020-12-27 15:02:52 +0900

    [fix]リファクタ

--- resources/js/components/Account/Index.vue
+++ resources/js/components/Account/Index.vue
@@ -19,69 +19,69 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        singleSelect: false,
-        selected: [],
-        headers: [
-          {
-            text: 'Dessert (100g serving)',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-        ],
-      }
-    },
-    mounted() {
-      this.fetchItem()
-    },
-    methods: {
-      async fetchItem() {
-        const response = await axios.get('api/account')
-        console.log(response)
-      },
+export default {
+  data () {
+    return {
+      singleSelect: false,
+      selected: [],
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+      ],
     }
+  },
+  mounted() {
+    this.fetchItem()
+  },
+  methods: {
+    async fetchItem() {
+      const { data } = await axios.get('api/user?api_token=XPhsWe3LMobdRER5hUF3OZWt7dug9IkgX9RhSAiNBN8EYWR9H7dtD1g73kSL')
+      console.log(data)
+    },
   }
+}
 </script>
 
